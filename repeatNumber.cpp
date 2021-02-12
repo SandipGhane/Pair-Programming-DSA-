351cbd980a0ae1b2be5c5dd3e5e25ce5f089fafe
SandipGhane <sandip.humation@gmail.com>
2021-02-12 23:10:00 +0530

    feat : find repeated and missing number in array

---
+++ repeatNumber.cpp
@@ -0,0 +1,37 @@
+/*
+    Problem Statement - Repeat and Missing Number
+    input - [0,1,5,4,3,1]
+    output - missing = 2, repeated=1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+void findMissingAndRepeated(int inpArr[], int size)
+{
+    int countArr[size] = {0};
+    int repeated, missing;
+    for (int i = 0; i < size; i++)
+    {
+        countArr[inpArr[i]]++;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        if (countArr[i] > 1)
+        {
+            repeated = i;
+        }
+        if (countArr[i] == 0)
+        {
+            missing = i;
+        }
+    }
+    cout << "Missing Number::" << missing << endl;
+    cout << "Repeated Number::" << repeated << endl;
+}
+
+int main()
+{
+    int inputArr[] = {0, 1, 5, 4, 3, 1};
+    int size = sizeof(inputArr) / sizeof(inputArr[0]);
+    findMissingAndRepeated(inputArr, size);
+}
