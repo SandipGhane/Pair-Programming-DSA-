6d328829b30bbf65db1202b5f4f678122d260651
SandipGhane <sandip.humation@gmail.com>
2021-02-12 23:50:39 +0530

    feat : print series

--- printSeries.cpp
+++ printSeries.cpp
@@ -2,3 +2,21 @@
     Given n=4;
     output = 12344321;
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+void printSeries(int n)
+{
+    if (n < 1)
+    {
+        return;
+    }
+    cout << n;
+    printSeries(n - 1);
+    cout << n;
+}
+int main()
+{
+    int n = 4;
+    printSeries(n);
+}
